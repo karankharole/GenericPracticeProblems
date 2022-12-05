882842ce3e0add067895638a743eeda7b1f8c785
karankharole <karankharole2000@gmail.com>
2022-12-05 23:37:26 +0530

    given max number at 1st position return the same number

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int maxNumber = GenericTestMax.MaxIntNumber(154, 64, 90);
+            Console.WriteLine(" Maximum Number is : " + maxNumber);
+        }
+    }
+}
