823ea30e3efde5ea95e2a0c6f19be317c00bc093
karankharole <karankharole2000@gmail.com>
2022-12-06 00:37:20 +0530

    refactor to create generic class to take in three variables of generic type

--- Program.cs
+++ Program.cs
@@ -3,12 +3,10 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-
-
-            Console.WriteLine("Maximum Interger Number is : " + GenericTestMax.MaxIntNumber<int>(30, 20, 10));
-            Console.WriteLine("Maximum Float Number is : " + GenericTestMax.MaxIntNumber<float>(30.3f, 45.20f, 76.10f));
-            Console.WriteLine("Maximum String Number is : " + GenericTestMax.MaxIntNumber<string>("78", "34", "10"));
+        { 
+            Console.WriteLine("Maximum Interger Number is : " + GenericTestMax<int>.MaxIntegerNumber(30, 20, 10));
+            Console.WriteLine("Maximum Float Number is : " + GenericTestMax<float>.MaxIntegerNumber(30.3f, 45.20f, 76.10f));
+            Console.WriteLine("Maximum String Number is : " + GenericTestMax<string>.MaxIntegerNumber("AAA", "CCCCC", "HHHH"));
         }
     }
 }
