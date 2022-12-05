da374f9e5a81b2798987a4a9de73be07f37a187f
karankharole <karankharole2000@gmail.com>
2022-12-06 00:51:47 +0530

    printing the max integer number among the three numbers

--- Program.cs
+++ Program.cs
@@ -3,10 +3,9 @@
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Maximum Interger Number is : " + GenericTestMax<int>.MaxIntegerNumber(30, 20, 10));
-            Console.WriteLine("Maximum Float Number is : " + GenericTestMax<float>.MaxIntegerNumber(30.3f, 45.20f, 76.10f));
-            Console.WriteLine("Maximum String Number is : " + GenericTestMax<string>.MaxIntegerNumber("AAA", "CCCCC", "HHHH"));
+        {
+            int maxNumber = GenericTestMax.MaxIntNumber(854, 764, 909);
+            Console.WriteLine("\n Maximum Number is : " + maxNumber);
         }
     }
 }
